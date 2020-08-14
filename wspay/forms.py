f699372f630b0ad511f36971ca752820b54bfa18
Vedran Vojvoda <vedran@pinkdroids.com>
2020-08-14 22:23:11 +0200

    Add response and expand request forms

--- wspay/forms.py
+++ wspay/forms.py
@@ -2,9 +2,16 @@ from django import forms
 
 
 class UnprocessedPaymentForm(forms.Form):
-    user_id = forms.IntegerField()
     cart_id = forms.CharField()
     price = forms.DecimalField()
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    city = forms.CharField(required=False)
+    zip_code = forms.CharField(required=False)
+    country = forms.CharField(required=False)
+    email = forms.EmailField(required=False)
+    phone = forms.CharField(required=False)
 
 
 class WSPaySignedForm(forms.Form):
@@ -16,3 +23,60 @@ class WSPaySignedForm(forms.Form):
     ReturnURL = forms.CharField(widget=forms.HiddenInput())
     CancelURL = forms.CharField(widget=forms.HiddenInput())
     ReturnErrorURL = forms.CharField(widget=forms.HiddenInput())
+    ReturnMethod = forms.CharField(widget=forms.HiddenInput())
+
+    # CUSTOMER DATA
+    CustomerFirstName = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerLastName = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerAddress = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerCity = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerZIP = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerCountry = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerEmail = forms.CharField(required=False, widget=forms.HiddenInput())
+    CustomerPhone = forms.CharField(required=False, widget=forms.HiddenInput())
+
+
+class WSPayBaseResponseForm(forms.Form):
+    ShoppingCartID = forms.CharField()
+    Success = forms.IntegerField()
+    ApprovalCode = forms.CharField(required=False)
+    Signature = forms.CharField()
+
+
+class WSPaySuccessErrorResponseForm(WSPayBaseResponseForm):
+    CustomerFirstName = forms.CharField(required=False)
+    CustomerSurname = forms.CharField(required=False)
+    CustomerAddress = forms.CharField(required=False)
+    CustomerCity = forms.CharField(required=False)
+    CustomerZIP = forms.CharField(required=False)
+    CustomerCountry = forms.CharField(required=False)
+    CustomerPhone = forms.CharField(required=False)
+    CustomerEmail = forms.CharField(required=False)
+    Lang = forms.CharField(required=False)
+    DateTime = forms.CharField(required=False)
+    Amount = forms.CharField()
+    ECI = forms.CharField(required=False)
+    PaymentType = forms.CharField(required=False)
+    PaymentPlan = forms.CharField(required=False)
+    ShopPostedPaymentPlan = forms.CharField(required=False)
+    ShopPostedLang = forms.CharField(required=False)
+    ShopPostedCreditCardName = forms.CharField(required=False)
+    ShopPostedPaymentMethod = forms.CharField(required=False)
+
+
+class WSPaySuccessResponseForm(WSPaySuccessErrorResponseForm):
+    STAN = forms.CharField(required=False)
+    Partner = forms.CharField(required=False)
+    WsPayOrderId = forms.CharField()
+    CreditCardNumber = forms.CharField(required=False)
+    ApprovalCode = forms.CharField()
+    ErrorMessage = forms.CharField(required=False)
+
+
+class WSPayErrorResponseForm(WSPaySuccessErrorResponseForm):
+    ErrorMessage = forms.CharField()
+    ErrorCodes = forms.CharField()
+
+
+class WSPayCancelResponseForm(WSPayBaseResponseForm):
+    ResponseCode = forms.CharField()
